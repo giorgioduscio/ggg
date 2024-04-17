21a6e32944e9b38697fbb3855d667b3f4b236764
giorgio <giorgio.duscio@gmail.com>
2024-04-17 11:41:52 +0200

    .

--- navigation.js
+++ navigation.js
@@ -2,14 +2,12 @@
 const body=document.body;
 const router=document.querySelector('router');
 
-// TODO PAGINE                                  -
+// fix PAGINE                                  -
 function homePage() { router.innerHTML=`<h1>homepage</h1>`; } 
-
 
 // TODO ROUTING                                 -
 // pagina principale
 body.addEventListener('click',function (event) {
-
     let pagName=false; 
 
     if (event.target.id!='') {
