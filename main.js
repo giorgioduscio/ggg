b97120efa3223437b09f5a819a0767c54523a51c
giorgio <giorgio.duscio@gmail.com>
2024-05-23 13:35:37 +0200

    API e router

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { navbar } from "./navbar.js";
+
 import { howAsync } from "./pages/howAsync.js";
 import { howDomManip } from "./pages/howDomManip.js";
 import { howManageDates } from "./pages/howMenageDates.js";
@@ -5,38 +7,60 @@ import { howRename } from "./pages/howRename.js";
 import { howToArray } from "./pages/howToArray.js";
 import { ordinations } from "./pages/ordinations.js";
 
+//TODO ROUTER
+function selectPage() {
+    // CREA NAVBAR
+    // PRENDI UN DATO
+    
+    // IL DATO RITORNA
+    return 0
+}
+
 function router(indexPage) {
     switch (indexPage) {
         case 0:
             return howRename()
-            break;
     
         case 1:
             return howToArray()
-            break;
     
         case 2:
             return ordinations()
-            break;
     
         case 3:
             return howManageDates()
-            break;
     
         case 4:
             return howDomManip()
-            break;
     
         case 5:
             return howAsync()
-            break;
     
         default:
-            return `<h1>Nessuna corrispondenza per ${indexPage}</h1>`;
-            break;
+            return `
+                <h1>Errore 404</h1>
+                <p>Nessuna corrispondenza per '${indexPage}'</p>
+            `;
     }
 }
 
-let indexPage= 10;
-document.getElementById('router').innerHTML= router(indexPage);
+document.getElementById('router').innerHTML= router( selectPage() );
 
+
+// IMPORTA API
+function  showJSON(json) {
+    console.log(json);
+
+    return `<div>
+        ${json.map(el=>{
+            return `<p>${el.name}</p> `
+        }).join('')}
+    </div>`;
+}
+
+function catchJSON() {
+    fetch('./jsonEse/bands.json')
+    .then(response=>response.json())
+    .then((json)=> console.log( showJSON(json)) )
+}
+catchJSON()
---
+++ navbar.js
@@ -0,0 +1,16 @@
+export function navbar() {
+    
+    return `
+        <nav>
+            <div>
+                <button>Pulsante</button>
+                <button>Pulsante</button>
+                <button>Pulsante</button>
+            </div>
+            <div>
+                <button>Pulsante</button>
+                <button>Pulsante</button>
+            </div>
+        </nav>
+    `;
+}
