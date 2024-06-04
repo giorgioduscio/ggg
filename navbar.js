05cd7d4ddf855c0f7d2360a91dbf4b13e37f4585
giorgio <giorgio.duscio@gmail.com>
2024-06-04 09:14:27 +0200

    dropdown

--- navbar.js
+++ navbar.js
@@ -2,10 +2,14 @@ import pages from './pages.js';
 
 export default function navbar() {
     document.querySelector("#navbar").innerHTML=`
-        <span>
-        ${pages.map(el=>{
-            return `<button id=${el.path}>${el.path}</button>`
-        }).join('')}
-        </span>
+        <div class="dropdown">
+            <button>Documenti</button>
+            
+            <div>
+            ${pages.map(el=>{
+                return `<button id=${ el.path }>${ el.path }</button>`
+            }).join('')}
+            </div>
+        </div>
     `;
 }
